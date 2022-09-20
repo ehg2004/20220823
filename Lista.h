7aeaa4de7bec8c51132b7f1fe1067155f6fddd2c
ehg2004 <enzoholzmanngaio@alunos.utfpr.edu.br>
2022-09-19 22:02:12 -0300

    Update Lista.h

--- Lista.h
+++ Lista.h
@@ -12,18 +12,86 @@ private:
 public:
     Lista(/* args */);
     ~Lista();
-    void
+    void inicializa();
+    bool incluaELemento(Elemento<TIPO>*pElemento);
+    bool incluaInfo(TIPO* pInfo);
+    void limpar();
+
+    //void listeInfos();
+    //NAO LISTA POR PADRAO
 };
 
-Lista::Lista(/* args */)
+Lista<TIPO>::Lista(/* args */)
 {
+    inicializa();
 }
 
-Lista::~Lista()
-{
-}
-
+Lista<TIPO>::~Lista()
 {
 
 }
+    template <class TIPO>
+    void Lista<TIPO>::inicializa()
+    {
+        pPrim =NULL;
+        pAtual=NULL;
+
+    }
+    template <class TIPO>
+    bool Lista<TIPO>::incluaELemento(Elemento<TIPO>*pElemento)
+    {
+        if (pAtual!=NULL)
+        {
+            pAtual->pProx = pElemento;
+            novoElemento->pAnt = pAtual;
+            pAtual=pELemento;
+        }
+        else
+        {
+            pAtual = pElemento;
+            pPrim=pELemento;
+        }
+
+    }
+
+    template <class TIPO>
+    bool Lista<TIPO>::incluaInfo(TIPO* pInfo)
+    {   
+        if(pInfo==NULL){return(false); printf("Erro, elemento nulo (info) na lista");}
+
+        Elemento<TIPO>* novoELemento = new Elemento<TIPO>*;
+        novoELemento->setInfo(pInfo);
+        if (pAtual!=NULL)
+        {
+            pAtual->pProx = novoElemento;
+            novoElemento->pAnt = pAtual;
+            pAtual=novoELemento;
+        }
+        else
+        {
+            pAtual = novoElemento;
+            pPrim=novoELemento;
+        }
+        return  true;
+    }
+    template <class TIPO>
+    void Lista<TIPO>::limpar()
+    {
+        
+    }
+    /*
+    template <class TIPO>
+    void Lista<TIPO>::listeInfos()
+    {   if(pPrim==NULL){printf("Lista vazia");}
+        
+        int j = 0;
+        for(Elemento<TIPO> i =pPrim;i!=NULL;i=i->pProx)
+        {
+            printf("ELemento %d na lista: %s \n",j++, i->pInfo->getNome());
+           
+        }
+
+
+    }
+    */
 #endif
